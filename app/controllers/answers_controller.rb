1933802f3de2d4f4cb5618e6e614a3cbfcfc0b1e
Antoine Magny <mr.magny+github@gmail.com>
2015-06-28 19:27:42 +0200

    result show but z-index not tested

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -18,6 +18,20 @@ class AnswersController < ApplicationController
   end
 
   def show
+    @answers = Answer.all
+    @questions = Question.all
+    x = 0
+    y = 0
+    @answers.each do |answer|
+      right_question_id = answer.question_id - 1
+      x += @questions[right_question_id].x_impact * answer.question_result.to_i
+      y += @questions[right_question_id].y_impact * answer.question_result.to_i
+      z += @questions[right_question_id].z_impact * answer.question_result.to_i
+      @result = [x,y,z]
+    end
+    # angle_calculation(@result[0], @result[1])
+    angle_calculation(-1,-1)
+    @category = calculate_category(@angle, @result[2])
   end
 
 
@@ -26,5 +40,33 @@ class AnswersController < ApplicationController
   def answer_params
     params.require(:answer).permit(:question_result, :token)
   end
+
+  def angle_calculation(x,y)
+    @angle = Math.atan2(x,y)
+  end
+
+  def calculate_category(angle,z)
+    if 0 <= angle && angle <= Math::PI/2
+      category = 1
+    elsif Math::PI/2 < angle && angle <= Math::PI
+      category = 2
+    elsif -Math::PI < angle && angle <= -Math::PI/2
+      category = 3
+    elsif -Math::PI/2 < angle && angle <= -0
+      category = 4
+    else
+      "oops"
+    end
+
+    if z >= 0
+      category += 4
+    end
+    category
+    # quart sup droit: 1 ou 5
+    # quart inf droit: 2 ou 6
+    # quart inf gauche: 3 ou 7
+    # quart sup gauche: 4 ou 8
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150624152706) do
     t.string   "text"
     t.float    "x_impact"
     t.float    "y_impact"
+    t.float    "z_impact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
