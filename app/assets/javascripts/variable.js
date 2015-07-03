39ff7f360665838f2b5ffa633d7e596056ced907
Malik Dauber <m.dauber@me.com>
2015-07-03 14:11:18 +0200

    Rocket

--- app/assets/javascripts/variable.js
+++ app/assets/javascripts/variable.js
@@ -25,7 +25,7 @@ var __scrollingSpeed = 800; // page scroll speed
 background style, set only one of below to true
 =================================================*/
 
-var __staticBackground = false; // set background image by css
+var __staticBackground = true; // set background image by css
 var __videoHeader = false; // video background toggle
 var __slideshowHeader = false; // image background toggle
 
